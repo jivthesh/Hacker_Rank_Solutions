59b964a4f14360d1414dd9f36a8a85f73865b0d6
jivthesh <jivtheshm@gmail.com>
2020-07-18 18:29:22 -0700

    write function eidted

--- Python/Introduction/writeAFunction.py
+++ Python/Introduction/writeAFunction.py
@@ -5,8 +5,20 @@ def is_leap(year):
     if year % 100 ==0:
         if year % 400 ==0:
             leap =True
-        elif year % 4 ==0:
+    elif year % 4 ==0:
             leap = True
     return leap
 
 year = int(raw_input())
+print is_leap(year)
+
+    
+
+
+
+
+
+
+
+
+
